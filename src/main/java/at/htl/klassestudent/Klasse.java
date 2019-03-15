ae0e3b493f7f6be0ee36e95f4145762709c7007f
schollsebastian <scholl.sebastian@gmx.at>
2019-03-15 10:50:34 +0100

    finished

--- src/main/java/at/htl/klassestudent/Klasse.java
+++ src/main/java/at/htl/klassestudent/Klasse.java
@@ -1,4 +1,55 @@
 package at.htl.klassestudent;
 
 public class Klasse {
+    private int klassenbezeichnung;
+    private Student[] students;
+
+    Klasse() {
+    }
+
+    Klasse(int klassenbezeichnung, Student[] students) {
+        this.klassenbezeichnung = klassenbezeichnung;
+        this.students = students;
+    }
+
+    public void sortBubble() {
+        for (int i = students.length; i > 1; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                if (students[j].compareTo(students[j + 1]) > 0) {
+                    swap(j, j + 1);
+                }
+            }
+        }
+    }
+
+    public void sortInsertion() {
+        Student[] newArray = new Student[students.length];
+
+        for (int i = 0; i < students.length; i++) {
+            int j = i - 1;
+
+            while (j >= 0 && newArray[j].compareTo(students[i]) > 0) {
+                newArray[j + 1] = newArray[j];
+                j--;
+            }
+
+            newArray[j + 1] = students[i];
+        }
+
+        students = newArray;
+    }
+
+    private void swap(int i, int j) {
+        Student help = students[i];
+        students[i] = students[j];
+        students[j] = help;
+    }
+
+    public int getKlassenbezeichnung() {
+        return klassenbezeichnung;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
 }
--- src/test/java/at/htl/klassestudent/KlasseStudentTest.java
+++ src/test/java/at/htl/klassestudent/KlasseStudentTest.java
@@ -1,7 +1,52 @@
 package at.htl.klassestudent;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
 public class KlasseStudentTest {
+    @Test
+    public void testA() {
+        Student[] arrayToSort = {
+                new Student("Philipp", "Edlinger"),
+                new Student("Lukas", "Starka"),
+                new Student("Sebastian", "Scholl"),
+                new Student("Jonas", "Wiesinger"),
+                new Student("Quirin", "Ecker"),
+                new Student("Maximilian", "Fuchs"),
+                new Student("Wilma", "Fuchs")
+        };
+        Student[] expected = arrayToSort.clone();
 
+        Klasse klasse = new Klasse(0, arrayToSort);
+        klasse.sortBubble();
+
+        Arrays.sort(expected);
+
+        assertThat(klasse.getStudents(), is(expected));
+    }
+
+    @Test
+    public void testB() {
+        Student[] arrayToSort = {
+                new Student("Philipp", "Edlinger"),
+                new Student("Lukas", "Starka"),
+                new Student("Sebastian", "Scholl"),
+                new Student("Jonas", "Wiesinger"),
+                new Student("Quirin", "Ecker"),
+                new Student("Maximilian", "Fuchs"),
+                new Student("Wilma", "Fuchs")
+        };
+        Student[] expected = arrayToSort.clone();
+
+        Klasse klasse = new Klasse(0, arrayToSort);
+        klasse.sortInsertion();
+
+        Arrays.sort(expected);
+
+        assertThat(klasse.getStudents(), is(expected));
+    }
 }
